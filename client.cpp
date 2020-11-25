0e609743e4b09c51505f198afe67610d63146c77
rayd512 <rdizon@ualberta.ca>
2020-11-24 18:56:33 -0700

    Commented

--- client.cpp
+++ client.cpp
@@ -11,6 +11,7 @@
 
 using namespace std;
 
+// Forward declare functions
 void check_input(int argc, char* argv[]);
 void Sleep( int n );
 void log_out(int int_time, string mode, FILE* fp);
@@ -20,23 +21,25 @@ double get_epoch_time();
 string get_logname();
 
 int main(int argc, char* argv[]) {
-	
+	// Init variables
 	int sock, num_trans = 0;
 	struct sockaddr_in server;
 	char message[1000], server_reply[1000];
 
+	// Check validity of input
 	check_input(argc, argv);
+
+	// Get the log file name
 	string log_name = get_logname();
-	// cout << log_name << endl;
-	//Create socket
+
+	// Create socket
 	sock = socket(AF_INET, SOCK_STREAM, 0);
-	
 	if (sock == -1) {
 		perror("Error: Could not create socket");
 		exit(EXIT_FAILURE);
 	}
 
-	// cout << "Socket created" << endl;
+	// Setup server
 	server.sin_addr.s_addr = inet_addr(argv[2]);
 	server.sin_family = AF_INET;
 	server.sin_port = htons(stoi(argv[1]));
@@ -47,61 +50,75 @@ int main(int argc, char* argv[]) {
 		exit(EXIT_FAILURE);
 	}
 	
-	// cout << "Connected" << endl;
+	// Open log file and write the header
 	FILE* fp = fopen(log_name.c_str(), "w+");
 	fprintf(fp, "Using port %s\n", argv[1]);
 	fprintf(fp, "Using server address %s\n", argv[2]);
 	fprintf(fp, "Host%s\n", log_name.c_str());
 
+	// Main loop for message passing between client and server
 	while(true) {
+		// Get message
 		memset(message, 0, 1000);
 		if (scanf("%s", message) != 1) {
 			break;
 		}
 		
+		// Handle sending transactions
 		if(message[0] == 'T') {
+			// Extract the int 
 			int trans_time = stoi(string(message).substr(1,
 									string(message).size()-1));
 
+			// Add the logfile name to the message
 			strcat(message, log_name.c_str());
 
-			//Send some data
-			// puts("Server send :");
-			// puts(message);
+			// Add transaction to the log
 			log_out(trans_time, "Trans", fp);
+			
+			// Send message to the server
 			if (send(sock, message, strlen(message), 0) < 0) {
 				puts("Send failed");
-				return 1;
+				exit(EXIT_FAILURE);
 			}
+
+			// Increment the number of transactions sent
 			num_trans++;
+
 			//Receive a reply from the server
 			memset(server_reply, 0, 1000);
 			if (recv(sock, server_reply, 1000, 0) < 0) {
-				puts("recv failed");
+				puts("Recv failed");
 				break;
 			}
-			// puts("Server reply: ");
-			// puts(server_reply);
+
+			// Process the reply from the server
 			process_reply(string(server_reply), fp, num_trans);
 		} else {
-
+			// Extract amount of units to sleep for
 			string sleep = string(message);
 			int sleep_time = stoi(sleep.substr(1, sleep.size()-1));
-			log_out(sleep_time, "Sleep", fp);
+
+			// Check validity of sleep time
 			if (sleep_time < 0 || sleep_time > 100) {
 				cout << "Invalid sleep time" << endl;
 				exit(EXIT_FAILURE);
 			}
 
+			// Log sleep
+			log_out(sleep_time, "Sleep", fp);
 			Sleep(sleep_time);
 		}
 	}
 
+	// write the footer and close the socket
 	write_footer(fp, num_trans);
 	close(sock);
 }
 
+// Checks if the input parameters are correct according to the specifications
 void check_input(int argc, char* argv[]) {
+	// Check amount of inputs passed
 	if(argc > 3) {
 		cout << "Error: Too many inputs" << endl;
 		cout << "client <port> <ip-address>" << endl;
@@ -112,6 +129,7 @@ void check_input(int argc, char* argv[]) {
 		exit(EXIT_FAILURE);
 	}
 
+	// Check is port number is valid
 	if(stoi(argv[1]) < 5000 || stoi(argv[1]) > 64000) {
 		cout << "Error: Invalid port number" << endl;
 		cout << "Port number must be in the range 5000 to 64000 inclusive" 
@@ -120,17 +138,21 @@ void check_input(int argc, char* argv[]) {
 	}
 }
 
+// Gets the log file name
 string get_logname() {
 	char hostname[256];
 
+	// Get the host name
 	if(gethostname(hostname, sizeof(hostname)) == -1) {
 		perror("Error: gethostname failure");
 		exit(EXIT_FAILURE);
 	}
 
+	// reurn hostname and the process id number in a string
 	return " " + string(hostname) + "." + to_string((int)getpid());
 }
 
+// Return the epoch time
 double get_epoch_time() {
 	struct timeval tv;
 	gettimeofday(&tv, NULL);
@@ -138,7 +160,9 @@ double get_epoch_time() {
 	return (double) (tv.tv_sec) + (double) (tv.tv_usec) / 1000000;
 }
 
+// Write to the log file
 void log_out(int int_time, string mode, FILE* fp) {
+	// Depending on the mode, write the correct output
 	if(mode == "Trans") {
 		fprintf(fp, "%.2f: Send (T%3d)\n", get_epoch_time(), int_time);
 	} else {
@@ -146,24 +170,29 @@ void log_out(int int_time, string mode, FILE* fp) {
 	}
 }
 
+// Write the summary of transactions
 void write_footer(FILE* fp, int num_trans) {
 	fprintf(fp, "Sent %d Transactions\n", num_trans);
 	fclose(fp);
 }
 
+// Process the reply from the server
 void process_reply(string server_reply, FILE* fp, int num_trans) {
 	string message, trans_string;
 	int trans_num;
+	// If reply is blank, the server is not available. Close the 
+	// footer and exit
 	if(server_reply == "") {
 		cout << "Error: Server is not available" << endl;
 		write_footer(fp, num_trans);
 		exit(EXIT_FAILURE);
 	}
 
+	// Split the output by space
 	istringstream stream(server_reply);
-
 	stream >> message >> trans_string;
 
+	// Write to log file if message is correct
 	if(message == "Done") {
 		trans_num = stoi(trans_string);
 		fprintf(fp, "%.2f: Recv (D%3d)\n", get_epoch_time(), trans_num);
--- server.cpp
+++ server.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 #include <sstream>
 #include <vector>
 #include <utility>
@@ -9,10 +10,12 @@
 #include <unistd.h>
 #include <sys/time.h>
 
+// Maximum number of connections
 #define MAX_CON 1000
 
 using namespace std;
 
+// Forward declare functions
 void check_input(int argc, char* argv[]);
 void Trans( int n );
 void log_output(string log_name, int trans_time, int trans_num, string mode);
@@ -22,7 +25,7 @@ void write_summary(vector<pair<string, int> > client_tracker,
 double get_epoch_time();
 
 int main(int argc, char *argv[]) {
-
+	// Init variables
 	int socket_desc, client_sock[MAX_CON], c, read_size, max_sd,
 		monitor, trans_num = 0;
 	struct sockaddr_in server, client;
@@ -31,26 +34,26 @@ int main(int argc, char *argv[]) {
 	fd_set sock_set;
 	vector<pair<string, int> > client_tracker;
 	double start_time = 0, end_time = 0;
+	
+	// Check validity of input passed from command line
 	check_input(argc, argv);
 
-	for(int i = 0; i < MAX_CON; i++) {
-		client_sock[i] = 0;
-	}
+	// Initialize array with zeroes
+	fill(begin(client_sock), end(client_sock), 0);
 
 	//Create socket
 	socket_desc = socket(AF_INET, SOCK_STREAM, 0);
-	
 	if (socket_desc == -1) {
 		perror("Error: Could not create socket");
 		exit(EXIT_FAILURE);
 	}
 	
-	//Prepare the sockaddr_in structure
+	// Prepare the sockaddr_in structure
 	server.sin_family = AF_INET;
 	server.sin_addr.s_addr = INADDR_ANY;
 	server.sin_port = htons(stoi(argv[1]));
 	
-	//Bind
+	// Bind
 	if( bind(socket_desc,(struct sockaddr *)&server, sizeof(server)) < 0) {
 		perror("Error: Bind Failed");
 		exit(EXIT_FAILURE);
@@ -58,23 +61,23 @@ int main(int argc, char *argv[]) {
 	
 	cout << "Using port " << stoi(argv[1]) << endl;
 
-	//Listen
+	// Listen
 	if(listen(socket_desc, MAX_CON) < 0) {
 		perror("Error: Listen Error");
 		exit(EXIT_FAILURE);
 	}
-
 	c = sizeof(struct sockaddr_in);
 
+	// Main loop of recieving clients and messages and sending messages
 	while(true) {
-
-		//Accept and incoming connection
-		// cout << "Waiting for incoming connections..." << endl;
-		
+		// Clear the set
 		FD_ZERO(&sock_set);
+		// Add socket_desc to the set
 		FD_SET(socket_desc, &sock_set);
+		// Hold max sd(int) for select function
 		max_sd = socket_desc;
 
+		// Add socket descriptors into the set
 		for(int i = 0; i < MAX_CON; i++) {
 			int child_sd = client_sock[i];
 
@@ -87,20 +90,23 @@ int main(int argc, char *argv[]) {
 			}
 		}
 
+		// Use select to monitor activity from sockets. 
 		monitor = select(max_sd + 1, &sock_set, NULL, NULL, &timeout); 
-		//accept connection from an incoming client
-		
+
 		if (monitor < 0) {
 			cout << "Error: Select error" << endl;
 		} else if (monitor == 0) {
-			// cout << "Timeout" << endl;
+			// Timeout has occured, exit the program
+			fflush(stdout);
 			close(socket_desc);
 			write_summary(client_tracker, start_time, end_time, trans_num);
 			exit(EXIT_SUCCESS);
 		}
 
+		// Check if data is avaible from socket_desc
 		if (FD_ISSET(socket_desc, &sock_set)) {
 
+			// Accept a new connection
 			int child_sock = accept(socket_desc, (struct sockaddr *)&client,
 								(socklen_t*)&c);
 
@@ -109,8 +115,7 @@ int main(int argc, char *argv[]) {
 				exit(EXIT_FAILURE);
 			}
 
-			// puts("Connection accepted");
-
+			// Add new socket to first empty place
 			for(int i = 0; i < MAX_CON; i++) {
 				if(client_sock[i] == 0) {
 					client_sock[i] = child_sock;
@@ -119,66 +124,74 @@ int main(int argc, char *argv[]) {
 			}
 		}
 		
-		
+		// Check for available data on other sd's
 		for(int i = 0; i < MAX_CON; i++) {
+			// Get sd and check for data availability
 			int sd = client_sock[i];
-
 			if (!FD_ISSET(sd, &sock_set))
 				continue;
 
+			// Recieve message from client
 			memset(client_message, 0, 1000);
 			if ((read_size = recv(sd, client_message, 1000, 0)) > 0 ) {
+
 				string transaction, log_name;
+
+				// Split message recieved by whitespace
 				string message = string(client_message);
 				istringstream stream(message);
-
 				stream >> transaction >> log_name;
 
+				// Increment the counter for the specific client
 				inc_tracker(client_tracker, log_name);
 
+				// Extract the int 
 				int trans_time = stoi(transaction.substr(1,
 														transaction.size()-1));
-				// cout << trans_time << endl;
 				trans_num++;
+
+				// Output to terminal before and after transaction
 				log_output(log_name, trans_time, trans_num, "Trans");
 				Trans(trans_time);
 				log_output(log_name, trans_time, trans_num, "Done");
-				//Send the message back to client
-				// printf( "Client recv: %s\n", client_message );
 
+				// Get the time of the first transaction
 				if(trans_num == 1) {
 					start_time = get_epoch_time();
 				}
+
+				// Get the time of the last transaction
 				end_time = get_epoch_time();
 
-
+				// Create a reply
 				sprintf(reply, "Done %d", trans_num);
 
+				// Send message to client
 				if(write(sd, reply, strlen(reply)) == -1) {
 					perror("Error: Writing Failed");
 					exit(EXIT_FAILURE);
 				}
 				
-				// printf( "Client sent: %s\n", client_message );
+				// Reset arrays
 				memset(client_message, 0, 1000);
 				memset(reply, 0, 1000);
 			}
 			
 			if(read_size == 0) {
-				// puts("Client disconnected");
-				fflush(stdout);
+				// Client has disconnected
 				close(sd);
+				// Clear the spot in the array
 				client_sock[i] = 0;
 			}
 			else if(read_size == -1) {
-				perror("recv failed");
+				perror("Error: recv failed");
 			}
 		}
 	}
 	return 0;
 }
 
-
+// Checks if the input from terminal meets the criteria
 void check_input(int argc, char* argv[]) {
 	if(argc > 2) {
 		cout << "Error: Too many inputs" << endl;
@@ -198,6 +211,7 @@ void check_input(int argc, char* argv[]) {
 	}
 }
 
+// Prints to stdout what the client is doing
 void log_output(string log_name, int trans_time, int trans_num, string mode) {
 	if(mode == "Done") {
 		printf("%.2f: # %2d (Done) from %s\n", get_epoch_time(),
@@ -208,6 +222,7 @@ void log_output(string log_name, int trans_time, int trans_num, string mode) {
 	}
 }
 
+// Returns the epoch time
 double get_epoch_time() {
 	struct timeval tv;
 	gettimeofday(&tv, NULL);
@@ -215,8 +230,10 @@ double get_epoch_time() {
 	return (double) (tv.tv_sec) + (double) (tv.tv_usec) / 1000000;
 }
 
+// Increments the amount of transactions each connected client has requested
 void inc_tracker(vector<pair<string, int> > client_tracker, string log_name) {
 	bool is_tracked = false;
+	// Increment client's transaction amount if in vector
 	for(int i = 0; i < (int)client_tracker.size(); i++) {
 		if(client_tracker[i].first == log_name) {
 			client_tracker[i].second++;
@@ -224,6 +241,7 @@ void inc_tracker(vector<pair<string, int> > client_tracker, string log_name) {
 			break;
 		}
 	}
+	// Add new client to vector
 	if(!is_tracked) {
 		pair <string, int> new_client;
 		new_client.first = log_name;
@@ -232,22 +250,29 @@ void inc_tracker(vector<pair<string, int> > client_tracker, string log_name) {
 	}
 }
 
+// Write the summary when server ends
 void write_summary(vector<pair<string, int> > client_tracker,
 				   double start_time, double end_time, int trans_num) {
 	double elapsed_time;
 
 	cout << endl;
 	cout << "SUMMARY" << endl;
+	
+	// Output client stats 
 	for(int i = 0; i < (int)client_tracker.size(); i++) {
 		printf("%4d transactions from %s\n", client_tracker[i].second,
 											 client_tracker[i].first.c_str());
 	}
+	
+	// Get the elapsed time. 
 	if (end_time == start_time) {
+		// Case if only one transaction happens. Grab the current time
 		elapsed_time = get_epoch_time() - start_time;
 	} else {
 		elapsed_time = (double)end_time - (double)start_time;
 	}
-	// printf("%.2f %.2f\n",end_time, start_time );
+
+	// print the trans/sec
 	printf("%4.1f transactions/sec (%d/%.2f) \n",
 			trans_num/elapsed_time, trans_num, elapsed_time);
 }
